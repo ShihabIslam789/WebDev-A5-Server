7ddd172208671294fb05f8f52016489f5db3e620
Shihab Islam <Shihab.Islam90@myhunter.cuny.edu>
2021-12-19 00:48:02 -0500

    server files half created and implemented

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -5,11 +5,22 @@ const Campus = db.define("campus", {
 
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    defaultValue: 'Campus Name'
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://techvig.net/wp-content/uploads/2020/11/Fixpii_email_35800da0131beebe44e2-Outlook-Error.png'
   },
 
-  description: {
+  address: {
     type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'unknown n/a'
+   },
+
+  description: {
+    type: Sequelize.TEXT('large'),
   }
 
 });
--- database/models/Student.js
+++ database/models/Student.js
@@ -5,14 +5,39 @@ const Student = db.define("student", {
 
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    defaultValue: 'Joe'
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    defaultvalue: ' Shmo'
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'johnsmith@email.com',
+    validate: {
+      isEmail: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV4Y6EDuXGzVum3fzCs_-xqcCh11DB7azm4cCahB1h-Hpd_IRBbXnZucHW-g_x-St0F0o&usqp=CAU'
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL,
+    defaultvalue: 4.0,
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
   }
 
 });
+
 
 module.exports = Student;
