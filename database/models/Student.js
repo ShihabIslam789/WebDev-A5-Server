f0ef573c13a6cbae19ed8e486a7415b0d03e8ae3
ShihabIslam789 <56773545+ShihabIslam789@users.noreply.github.com>
2021-12-19 00:52:04 -0500

    Update Student.js
    incorrect values found and edited

--- database/models/Student.js
+++ database/models/Student.js
@@ -1,43 +1,39 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
 const Student = db.define("student", {
-
   firstname: {
     type: Sequelize.STRING,
     allowNull: false,
-    defaultValue: 'Joe'
   },
 
   lastname: {
     type: Sequelize.STRING,
     allowNull: false,
-    defaultvalue: ' Shmo'
   },
 
   email: {
     type: Sequelize.STRING,
     allowNull: false,
-    defaultValue: 'johnsmith@email.com',
-    validate: {
-      isEmail: true
-    }
+    defaultValue: "N/A",
+    isEmail: true,
   },
-  imageUrl: {
+
+  imgurl: {
     type: Sequelize.STRING,
-    defaultValue: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV4Y6EDuXGzVum3fzCs_-xqcCh11DB7azm4cCahB1h-Hpd_IRBbXnZucHW-g_x-St0F0o&usqp=CAU'
+    defaultValue:
+      "https://cdn-icons-png.flaticon.com/128/2748/2748558.png",
+    validate: {
+      isUrl: true,
+    },
   },
 
   gpa: {
-    type: Sequelize.DECIMAL,
-    defaultvalue: 4.0,
-    validate: {
-      min: 0.0,
-      max: 4.0
-    }
-  }
-
+    type: Sequelize.STRING,
+    defaultValue: "N/A",
+    isDecimal: true,
+    validate: {min: 0.0, max: 4.0 },
+  },
 });
-
 
 module.exports = Student;
